06ddbd476986afd128626622428d5ed7e1a3b4e7
蜘蛛啊全是腿儿 <446783737@qq.com>
2020-11-05 20:34:47 +0800

    可以实现简单的分页查询

--- src/test/java/ttt/MyTest.java
+++ src/test/java/ttt/MyTest.java
@@ -3,18 +3,35 @@ package ttt;
 import com.kk.pojo.Books;
 import com.kk.service.BookService;
 import org.junit.Test;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.awt.print.Book;
 
 public class MyTest {
 
     @Test
     public void test(){
-        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
-        BookService bookService = (BookService) context.getBean("bookService");
+        ApplicationContext context = new ClassPathXmlApplicationContext("classpath:applicationContext.xml");
+        BookService bookService = context.getBean("bookService",BookService.class);
+        Books b = new Books();
+        b.setBookName("asdsa");
+        b.setCounts(55);
+        b.setDetail("卡山谷的比较火爆");
+        bookService.addBook(b);
+        int i = 1/0;
         for (Books book : bookService.fyAllBook()
              ) {
             System.out.println(book);
         }
 
     }
+
+
+
+
+
+
+
+
 }
